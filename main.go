e541af015d1c8825e9d368db37838bac4e6977c8
VTRyo <vtryo.work@gmail.com>
2022-06-26 08:38:21 +0900

    処理の追加

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"math/rand"
+	"net/http"
+	"time"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
@@ -17,6 +22,31 @@ var (
 		prometheus.GaugeOpts{
 			Name: "first_number",
 			Help: "first Number",
-		}, []string{"exapmle", "gage"},
+		}, []string{"example", "gage"},
 	)
 )
+
+func count() {
+	// Labelsのmap[string]stringはvarで定義したものと一致する必要がある
+	for {
+		firstCounter.With(prometheus.Labels{"count": "increment", "example": "true"}).Inc()
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func setRandomValue() {
+	for {
+		rand.Seed(time.Now().UnixNano())
+		n := -1 + rand.Float64()*2
+		firstGage.With(prometheus.Labels{"gage": "integer", "example": "true"}).Set(n)
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func main() {
+	go count()
+	go setRandomValue()
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":2112", nil)
+}
